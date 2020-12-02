9637f6549d8fdbb2ed4a9f8ab609eb836872c94d
RFSFreitas <rogerio.francisco.freitas@hotmail.com>
2020-12-01 21:22:11 -0300

    feat: user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,14 @@
+import { UserData } from './user-data'
+describe('Register user on mailin list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailinList(repo)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // const response = await usecase.RegisterUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('any@email.com')
+    // expect((await user).name).toBe('any_name')
+  })
+})
